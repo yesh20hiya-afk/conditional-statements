5cd5d665ada60d0a5aca96c3592694e82ea013a7
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-01-14 20:45:45 -0500

    v3

---
+++ activity3.py
@@ -0,0 +1,10 @@
+i = int(input("Enter an number: "))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
+
+print ("i'm out of if-else block")
